1c823adc92a0deed7476b00e17f0db6d3d70105f
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-02-25 14:29:17 +0200

    feat(list-books): fetch and display all books on HomePage

--- frontend/src/pages/BookPage.jsx
+++ frontend/src/pages/BookPage.jsx
@@ -1,4 +1,66 @@
 const BookPage = () => {
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      try {
+        const res = await fetch(`/api/books/${id}`);
+        if (!res.ok) throw new Error("Network response was not ok");
+        const data = await res.json();
+        setBook(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBook();
+  }, [id]);
+
+  {
+    book && (
+      <div>
+        <h2>{book.title}</h2>
+        <p>Author: {book.author}</p>
+        <p>ISBN: {book.isbn}</p>
+        <p>Publisher: {book.publisher}</p>
+        <p>Genre: {book.genre}</p>
+        <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+        <p>
+          Due Date:{" "}
+          {book.availability.dueDate
+            ? new Date(book.availability.dueDate).toLocaleDateString()
+            : "—"}
+        </p>
+        <p>Borrower: {book.availability.borrower || "—"}</p>
+      </div>
+    );
+  }
+
+  const deleteBook = async (bookId) => {
+    try {
+      const res = await fetch(`/api/books/${bookId}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) throw new Error("Failed to delete book");
+    } catch (error) {
+      console.error("Error deleting book:", error);
+    }
+  };
+
+  const onDeleteClick = (bookId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this book?",
+    );
+    if (!confirm) return;
+    deleteBook(bookId);
+    navigate("/");
+  };
+
   return (
     <div className="book-preview">
       <>
@@ -11,6 +73,8 @@ const BookPage = () => {
         <p>Due Date: </p>
         <p>Borrower: </p>
       </>
+      <button onClick={() => navigate("/")}>Back</button>
+      <button onClick={() => onDeleteClick(book._id)}>Delete</button>
     </div>
   );
 };
