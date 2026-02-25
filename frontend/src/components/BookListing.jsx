5919a62c76288a8ce3712fa2679f4fc3a9a3f9dd
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-02-25 14:03:50 +0200

    feat(list-book): fech and display all books on home page

--- frontend/src/components/BookListing.jsx
+++ frontend/src/components/BookListing.jsx
@@ -1,14 +1,28 @@
-const BookListing = () => {
+const BookListings = ({ books }) => {
+   
+
+
+  const BookListing = ({ book }) => {
   return (
     <div className="book-preview">
-      <h2>Book Title:</h2>
-      <p>Author:</p>
-      <p>ISBN:</p>
-      <p>Publisher:</p>
-      <p>Genre:</p>
-      <p>Available:</p>
+      <h2>{book.title}</h2>
+      <p>Author: {book.author}</p>
+      <p>ISBN: {book.isbn}</p>
+      <p>Publisher: {book.publisher}</p>
+      <p>Genre: {book.genre}</p>
+      <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
     </div>
   );
 };
 
-export default BookListing;
+
+
+
+  return (
+    <div className="book-list">
+      {books.map((book) => (
+        <BookListing key={book.id} book={book} />
+      ))}
+    </div>
+  );
+};
